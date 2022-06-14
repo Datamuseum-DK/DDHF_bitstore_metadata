f3d7a54d28d8f86e64ba5f918ac516a23dc4ffea
Poul-Henning Kamp <phk@FreeBSD.org>
2022-06-14 23:39:52 +0000

    ISSN and ISBN checksum calculations

--- ddhf_bitstore_metadata/sections/document.py
+++ ddhf_bitstore_metadata/sections/document.py
@@ -43,16 +43,75 @@ class ISSN(Field):
 
     def validate(self):
         yield from super().validate()
-        if not re.match('^[0-9]{4}-[0-9]{4}$', self.val):
-            yield self.complaint("ISSN format is not ####-#### (%s)" % self.val)
+        if re.match('^[0-9]{4}-[0-9]{3}[0-9X]$', self.val):
+            csum = 0
+            for i, j in (
+                ( self.val[0], 8),
+                ( self.val[1], 7),
+                ( self.val[2], 6),
+                ( self.val[3], 5),
+                ( self.val[5], 4),
+                ( self.val[6], 3),
+                ( self.val[7], 2),
+            ):
+                csum += int(i, 10) * j
+            csum = csum % 11
+            csum = 11 - csum
+            if self.val[8] != "?123456789X0"[csum]:
+                yield self.complaint("ISSN checksum error (%s)" % self.val)
+            return
+
+        yield self.complaint("ISSN format is not ####-#### (%s)" % self.val)
 
 class ISBN(Field):
     ''' International Standard Book Number '''
 
     def validate(self):
         yield from super().validate()
-        if not re.match('^[0-9]{10}$', self.val):
-            yield self.complaint("ISBN format is not ########## (%s)" % self.val)
+        if re.match('^[0-9]{9}[0-9X]$', self.val):
+            csum = 0
+            for i, j in (
+                ( self.val[0], 10),
+                ( self.val[1], 9),
+                ( self.val[2], 8),
+                ( self.val[3], 7),
+                ( self.val[4], 6),
+                ( self.val[5], 5),
+                ( self.val[6], 4),
+                ( self.val[7], 3),
+                ( self.val[8], 2),
+            ):
+                csum += int(i, 10) * j
+            csum = csum % 11
+            csum = 11 - csum
+            if self.val[9] != "?123456789X0"[csum]:
+                yield self.complaint("ISBN10 checksum error (%s)" % self.val)
+            return
+
+        if re.match('^[0-9]{12}[0-9X]$', self.val):
+            csum = 0
+            for i, j in (
+                ( self.val[0], 1),
+                ( self.val[1], 3),
+                ( self.val[2], 1),
+                ( self.val[3], 3),
+                ( self.val[4], 1),
+                ( self.val[5], 3),
+                ( self.val[6], 1),
+                ( self.val[7], 3),
+                ( self.val[8], 1),
+                ( self.val[9], 3),
+                ( self.val[10], 1),
+                ( self.val[11], 3),
+            ):
+                csum += int(i, 10) * j
+            csum = csum % 10
+            csum = 10 - csum
+            if self.val[12] != "?1234567890"[csum]:
+                yield self.complaint("ISBN13 checksum error (%s)" % self.val)
+            return
+
+        yield self.complaint("ISBN format is not ########## (%s)" % self.val)
 
 class Document(Section):
     ''' Document sections '''
